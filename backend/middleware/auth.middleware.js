bdb5742d39a27dc87f2db007e4a52c52f4bfba37
omp12345 <opsgp1999@gmail.com>
2023-12-28 00:58:17 -0800

    ok

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -8,35 +8,11 @@ const auth = (req, res, next) => {
   if(blacklist.includes(token)){
       next()
   }
-    try {
-      const token = req.headers.authorization.split(' ')[1];
-   
-      const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-     console.log(decodedToken)
-      req.body.userId = decodedToken.userId ;
-      req.body.username=decodedToken.username
-      next();
-    } catch (error) {
-      res.status(401).json({ message: 'Authentication failed' });
-    }
-  };
-  const isAdmin = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
-   
-    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-    console.log(decodedToken)
-    if ( decodedToken.role === 'admin') {
-     
-      return next(); 
-    }else{
-      return res.status(401).json({ error: error.message});
-    }
     
-  }
-  
+  };
+ 
  
   
   module.exports={
-   auth,
-   isAdmin
+   auth
   }
